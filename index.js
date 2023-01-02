7fdfd121f44a09f4c499d4c4bbff1735f0aee3c6
Sufyan Hussein <sufianb.contact@gmail.com>
2023-01-02 22:16:57 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+const superbowlWin = array => {
+    let result = undefined;
+    result = array.find(element => element.result === "W");
+    return result !== undefined ? result.year :undefined;
+}
